07bfbb0465b1afd3a7f1ba85f6ed7bfa15b3704c
Lucia <lucia.jmunoz@alumnos.upm.es>
2023-04-16 18:33:56 +0200

    routing a home

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
-const routes: Routes = [
-  { path: "", component: AppComponent, pathMatch: "full" }
-=======
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: "", component: AppComponent, pathMatch: "full" },
   { path: "home", component: HomeComponent}
->>>>>>> 3017e014be1cd9038a9645ab156278ee636e7138
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
